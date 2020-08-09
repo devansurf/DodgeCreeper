5d4382b7379b4b88a19b05a9b81628c9af994764
devan <devan@DESKTOP-KG23R88>
2020-08-09 19:57:54 -0400

    Player moves to spectator when they die.

--- DodgeCreeper/src/me/devsdevelop/listeners/OnPlayerDamageListener.java
+++ DodgeCreeper/src/me/devsdevelop/listeners/OnPlayerDamageListener.java
@@ -8,6 +8,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 import me.devsdevelop.utils.Utils;
+import me.devsdevelop.utils.sound.SoundManager;
+import me.devsdevelop.utils.sound.SoundType;
 
 public class OnPlayerDamageListener implements Listener{
 
@@ -17,6 +19,7 @@ public class OnPlayerDamageListener implements Listener{
 		if (event.getDamage() >= player.getHealth()) {
 			player.setGameMode(GameMode.SPECTATOR);
 			Bukkit.broadcastMessage(Utils.chat("&aThe player &6" + player.getName() + " has been eliminated"));
+			SoundManager.sendSound(player, player.getLocation(), SoundType.BLEED);
 		}
 		
 		
